530ee9ddc351cad91c7f1acde25c5638df46ab89
Marlos Augusto <marlosaugusto@outlook.com>
2019-08-21 10:28:24 -0300

    test fetch to backend

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  function handleTest() {
+    fetch("https://rh-lab-backend.herokuapp.com/test")
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +21,7 @@ function App() {
         >
           Learn React
         </a>
+        <button onClick={handleTest()}>Test Back</button>
       </header>
     </div>
   );
