165b4174eaf776b9c41636704dab59ec7f835d8d
MarlosAugusto <marlos.gomes@codengage.com>
2019-11-22 21:12:42 -0200

    feat(service): service to AUTH

---
+++ src/services/FB.js
@@ -0,0 +1,42 @@
+import firebase from "@firebase/app";
+import "@firebase/auth";
+import "@firebase/storage";
+
+export const SignIn = async (email, password) => {
+  let success = false;
+  let error = null;
+  await firebase
+    .auth()
+    .signInWithEmailAndPassword(email, password)
+    .then(() => (success = true))
+    .catch(err => (error = err.code));
+
+  if (error) {
+    return error;
+  }
+
+  return success;
+};
+
+export const SignOut = async () => {
+  let success = false;
+  await firebase.auth().signOut()
+    .then(() => {
+      success = true;
+    });
+
+  return success;
+
+};
+
+export const SignUp = async (email, password) => {
+  let success = false;
+  await firebase
+    .auth()
+    .createUserWithEmailAndPassword(email, password)
+    .then(() => {
+      success = true;
+    });
+
+  return success;
+};
